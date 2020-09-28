a632e7384862a0c3fddc6bce8899793d788c6e05
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-09-28 20:37:19 +0300

    Revert "made as in the courseÑ"
    This reverts commit b2aebcc4b9050aac41ab290575e11d5b0b060394.

--- dist/js/script.js
+++ dist/js/script.js
@@ -183,31 +183,17 @@ window.addEventListener('DOMContentLoaded', () => {
   const modalClose = document.querySelector('[data-close]');
 
   function showModal() {
-    modal.classList.add('show');
-    modal.classList.remove('hide');
-    document.body.style.overflow = 'hidden';
+    modal.style.display = 'block';
   }
 
   function hideModal() {
-    modal.classList.add('hide');
-    modal.classList.remove('show');
-    document.body.style.overflow = '';
+    modal.style.display = 'none';
   }
 
   modalOpen.forEach(item => {
     item.addEventListener('click', showModal);
   });
   modalClose.addEventListener('click', hideModal);
-  modal.addEventListener('click', e => {
-    if (e.target === modal) {
-      hideModal();
-    }
-  });
-  document.addEventListener('keydown', e => {
-    if (e.code === 'Escape' && modal.classList.contains('show')) {
-      hideModal();
-    }
-  });
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -97,15 +97,11 @@ window.addEventListener('DOMContentLoaded', () => {
   const modalClose = document.querySelector('[data-close]');
 
   function showModal() {
-    modal.classList.add('show');
-    modal.classList.remove('hide');
-    document.body.style.overflow = 'hidden';
+    modal.style.display = 'block';
   }
 
   function hideModal() {
-    modal.classList.add('hide');
-    modal.classList.remove('show');
-    document.body.style.overflow = '';
+    modal.style.display = 'none';
   }
 
   modalOpen.forEach((item) => {
@@ -113,16 +109,4 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   modalClose.addEventListener('click', hideModal);
-
-  modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-      hideModal();
-    }
-  });
-
-  document.addEventListener('keydown', (e) => {
-    if (e.code === 'Escape' && modal.classList.contains('show')) {
-      hideModal();
-    }
-  });
 });
