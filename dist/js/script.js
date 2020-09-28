b42035c9228b660c2fa29d03422574dfa7ebdd57
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-09-28 20:39:37 +0300

    made as in the course

--- dist/js/script.js
+++ dist/js/script.js
@@ -183,17 +183,31 @@ window.addEventListener('DOMContentLoaded', () => {
   const modalClose = document.querySelector('[data-close]');
 
   function showModal() {
-    modal.style.display = 'block';
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
   }
 
   function hideModal() {
-    modal.style.display = 'none';
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
   }
 
   modalOpen.forEach(item => {
     item.addEventListener('click', showModal);
   });
   modalClose.addEventListener('click', hideModal);
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      hideModal();
+    }
+  });
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      hideModal();
+    }
+  });
 });
 
 /***/ })
